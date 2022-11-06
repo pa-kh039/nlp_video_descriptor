4a6b2400f0f8605e7e682200ad616fb72e92d8d2
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2022-11-06 19:47:19 +0530

    added few lines to integrate checkking of image similarity

--- video_divider.py
+++ video_divider.py
@@ -4,29 +4,36 @@ import cv2
 
 # Function to extract frames
 def FrameCapture(path):
-	
+	global previousimage
+	# global threshold
+	# 	
 	# Path to video file
 	vidObj = cv2.VideoCapture(path)
 
 	# Used as counter variable
 	count = 0
+	# counter2=0
 
 	# checks whether frames were extracted
 	success = 1
-
+    
 	while success:
-
 		# vidObj object calls read
 		# function extract frames
 		success, image = vidObj.read()
-
-		# Saves the frames with frame-count
-		cv2.imwrite("frame%d.jpg" % count, image)
-
-		count += 1
+		#threshold is a hyperparameter which is to be tuned regularly
+		if previousimage==None or diff(previousimage,image)>threshold:
+			#Saves the frames with frame-count
+			cv2.imwrite("frame%d.jpg" % count, image)
+			count+= 1
+	print(success,"at the end")
+		
 
 # Driver Code
 if __name__ == '__main__':
 
 	# Calling the function
-	FrameCapture("/home/harshith/Desktop/NLP_Project/a.mp4")
+	previousimage=None
+	# threshold=1234567
+	path=input("Enter full path of the video whose description is to be generated:")
+	FrameCapture(path)
